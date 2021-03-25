24a291303a8b3703503d4758ab9920f7ab6d2fe9
Ayesha <fxolbird@gmail.com>
2021-03-25 11:31:58 -0700

    wrapped

---
+++ Wrapper.java
@@ -0,0 +1,20 @@
+public  class Wrapper {
+    public static void main(String args[]){  
+        //Converting int primitive into Integer object  
+        int num=100;  
+        Integer obj=Integer.valueOf(num);  
+    
+        System.out.println(num+ " "+ obj);  
+       }
+       // object
+
+    //    public static void main(String args[]){  
+    //     //Creating Wrapper class object 
+    //     Integer obj = new Integer(100);  
+            
+    //     //Converting the wrapper object to primitive
+    //     int num = obj.intValue();
+    
+    //     System.out.println(num+ " "+ obj);  
+    //    }
+    }
